e67a1358f50d6423d955f38f8671c595b337fd33
Barry_Hacker <thiernohacker@gmail.com>
2020-02-20 23:46:37 +0000

    Ajout de la liste deroulante dans les fichiers inscription et connexion. Finition du fichier models.py

--- Planning_O/PlanninGO/models.py
+++ Planning_O/PlanninGO/models.py
@@ -38,14 +38,24 @@ class Professeur(models.Model):
 class Salle(models.Model):
     nom_salle = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nom_salle
+
 class TextBook(models.Model):
+    nom_cahier = models.CharField(max_length=25, default="", editable=False)
     nom_niveau = models.ForeignKey('Niveau', on_delete=models.CASCADE)
     gerant = models.ForeignKey(Professeur, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Cahier de texte concernant '+self.nom_cahier+' de la classe'+self.nom_niveau+'gere par '+self.gerant
 
 class Niveau(models.Model):
     nom_niveau = models.CharField(max_length=30, primary_key=True)
     responsable_pedagogique = models.ForeignKey(Professeur, on_delete=models.CASCADE)
     cahier = models.ForeignKey(TextBook, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.nom_niveau+' gere par '+self.responsable_pedagogique
 
 
 
@@ -58,10 +68,16 @@ class Cours(models.Model):
     duree = models.IntegerField()
     etudiant = models.ManyToManyField(Etudiant, through='Note')
 
+    def __str__(self):
+        return self.titre+' de Monsieur '+self.professeur+' avec un duree en heure de '+self.duree
+
 class Note(models.Model):
     etudiant = models.ForeignKey(Etudiant, on_delete=models.CASCADE)
     cours = models.ForeignKey(Cours, on_delete=models.CASCADE)
     valeur = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return 'Note de l\'etudiant '+self.etudiant+' concernant le cours '+self.cours+' : '+self.valeur
 
 
 # Create your models here.
